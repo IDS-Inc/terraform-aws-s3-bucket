eed9d6e3713ceef8d4b06f5a61c98fef4455e02d
Adrian Herrera <simplyadrian@gmail.com>
2019-04-24 17:39:42 +0000

    adding version tag

--- main.tf
+++ main.tf
@@ -101,7 +101,7 @@ resource "aws_s3_bucket" "default" {
 }
 
 module "s3_user" {
-  source       = "git::https://github.com/IDS-Inc/terraform-aws-iam-s3-user.git?ref=master"
+  source       = "git::https://github.com/IDS-Inc/terraform-aws-iam-s3-user.git?ref=0.3.3"
   namespace    = "${var.namespace}"
   stage        = "${var.stage}"
   name         = "${var.name}"
